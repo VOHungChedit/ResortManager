07f5aa86865345e17c9a9035521ce3b7cc4b8107
ngochung <kiemhoss30032000@gmail.com>
2023-03-16 10:36:25 +0700

    Create DbContext and Migrations

---
+++ src/ResortMan.Data/ResortDbContext.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.EntityFrameworkCore;
+using ResortMan.Entities;
+
+namespace ResortMan.Data;
+
+public class ResortDbContext : DbContext
+{
+    public ResortDbContext(DbContextOptions<ResortDbContext> options) : base(options)
+    {
+    }
+
+    public DbSet<Booking> Bookings { get; set; } = null!;
+
+    public DbSet<Accomodation> Accomodations { get; set; } = null!;
+
+    public DbSet<AccomodationPackage> AccomodationsPackages { get; set; } = null!;
+
+    public DbSet<AccomodationType> AccomodationTypes { get; set; } = null!;
+}
