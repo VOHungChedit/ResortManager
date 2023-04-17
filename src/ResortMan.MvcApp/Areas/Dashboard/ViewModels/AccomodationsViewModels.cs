82a8cb872c5dd5cce7e415ff6a92db039d9bfecd
ngochung <kiemhoss30032000@gmail.com>
2023-04-17 13:47:53 +0700

    Edit : accomodation

---
+++ src/ResortMan.MvcApp/Areas/Dashboard/ViewModels/AccomodationsViewModels.cs
@@ -0,0 +1,31 @@
+﻿using ResortMan.Entities;
+using ResortMan.ViewModels;
+
+namespace ResortMan.MvcApp.Areas.Dashboard.ViewModels
+{
+    
+        public class AccomodationsListingModel
+        {
+            public IEnumerable<Accomodation> Accomodations { get; set; }
+
+            public int? AccomodationPackageId { get; set; }
+            public IEnumerable<AccomodationPackage> AccomodationPackages { get; set; }
+            public string SearchTerm { get; set; }
+
+            //public Pager Pager { get; set; }
+        }
+
+        public class AccomodationActionModel
+        {
+            public int Id { get; set; }
+
+            public int AccomodationPackageId { get; set; }
+            public AccomodationPackage AccomodationPackage { get; set; }
+
+            public string Name { get; set; }
+            public string Description { get; set; }
+
+            public IEnumerable<AccomodationPackage> AccomodationPackages { get; set; }
+        }
+    
+}
